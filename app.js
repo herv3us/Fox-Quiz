0048323a0aaee20235e15dbe57b960496a22509f
sarablom <sara.cecilia.blom@gmail.com>
2021-03-11 11:01:54 +0100

    Added questions in JS

---
+++ app.js
@@ -0,0 +1,52 @@
+const foxquiz = [
+    {
+        question: '1. Vad heter rödräv på latin?',
+        answers: {
+        answer1: 'Vulpes vulpes',
+        answer2: 'Ulves ulves',
+        answer3: 'Foxes foxes'
+    },
+        correctAnswer: 'Vulpes vulpes',
+    },
+
+    {
+        question: '2. Hur mycket väger en räv?',
+        answers: {
+        answer1: '5-10 kg',
+        answer2: '10-15 kg',
+        answer3: '15-20 kg'
+    },
+        correctAnswer: '5-10 kg',
+    },
+
+    {
+        question: '3. Hur många ungar får en rävhona per kull?',
+        answers: {
+        answer1: '1-3',
+        answer2: '2-5',
+        answer3: '4-7'
+    },
+        correctAnswer: '4-7',
+    },
+    
+    {
+        question: '4. Vilka är rävens viktigaste sinnen?',
+        answers: {
+        answer1: 'Hörselsinnet och luktsinnet',
+        answer2: 'Smaksinnet och hörselsinnet',
+        answer3: 'Smaksinnet'
+    },
+        correctAnswer: 'Hörselsinnet och luksinnet',
+    },
+
+    {
+        question: '5. Med vilka egenskaper brukar räven benämnas i fabler?',
+        answers: {
+        answer1: 'Rolig och smart',
+        answer2: 'Snabb och smidig',
+        answer3: 'Klok och listig'
+    },
+        correctAnswer: 'Klok och listig',
+    },
+    
+]
